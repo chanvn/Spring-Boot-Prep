4d3be00113c7ecb5a41c04b0831e711a51ce8eec
Chanvir <chanvir.nijjar@gmail.com>
2021-08-30 18:39:45 -0400

    Added main method implementation

--- Annotations-1/src/main/java/com/springdemoannotations/demo/AnnotationDemoApp.java
+++ Annotations-1/src/main/java/com/springdemoannotations/demo/AnnotationDemoApp.java
@@ -1,7 +1,21 @@
 package com.springdemoannotations.demo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationDemoApp {
     public static void main(String[] args) {
-        
+
+        // read spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // get bean from spring container
+        Coach tCoach = context.getBean("tennisCoach", Coach.class);
+
+        // call method on bean
+        System.out.println(tCoach.getDailyWorkout());
+
+        //close context
+        context.close();
     }
 }
