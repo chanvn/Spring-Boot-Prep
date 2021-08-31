33a7cab939db1c20b929599207b2302c06e1d083
Chanvir <chanvir.nijjar@gmail.com>
2021-08-30 22:32:08 -0400

    Implement random fortune service class

--- Annotations-1/src/main/java/com/springdemoannotations/demo/RandomFortuneService.java
+++ Annotations-1/src/main/java/com/springdemoannotations/demo/RandomFortuneService.java
@@ -2,11 +2,22 @@ package com.springdemoannotations.demo;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService {
 
+    // create an array of strings
+    private String[] data = {
+            "Keep your work safe", "Work hard", "Journey is the reward"
+    };
+
+    // create an RNG
+    private Random rng = new Random();
+
     @Override
     public String getFortune() {
-        return null;
+        int randomNum = rng.nextInt(data.length);
+        return data[randomNum];
     }
 }
