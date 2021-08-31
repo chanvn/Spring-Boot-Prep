605374d4f4209e8b939a1a5086ea0a74e05d3a5e
Chanvir <chanvir.nijjar@gmail.com>
2021-08-30 22:48:53 -0400

    Use file reading to add new implementation of FortuneService interface and @Qualify to resolve dependency

--- Annotations-1/src/main/java/com/springdemoannotations/demo/DatabaseFortuneService.java
+++ Annotations-1/src/main/java/com/springdemoannotations/demo/DatabaseFortuneService.java
@@ -2,10 +2,44 @@ package com.springdemoannotations.demo;
 
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 @Component
 public class DatabaseFortuneService implements FortuneService {
+    private String fileName = "C:\\Users\\chanv\\Documents\\Self-Teach\\Spring-boot\\Spring-Boot-Prep\\Annotations-1\\src\\main\\resources\\fortunes.txt";
+    ArrayList<String> fortunes;
+
+    private Random rng = new Random();
+
+    public DatabaseFortuneService(){
+        File theFile = new File(fileName);
+        fortunes = new ArrayList<String>();
+
+        System.out.println("File exists?: "+theFile.exists());
+
+        // read fortunes from file
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(theFile))) {
+
+            String tempLine;
+
+            while ((tempLine = br.readLine()) != null) {
+                fortunes.add(tempLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public String getFortune() {
-        return null;
+        int index = rng.nextInt(fortunes.size());
+        return fortunes.get(index);
     }
 }
