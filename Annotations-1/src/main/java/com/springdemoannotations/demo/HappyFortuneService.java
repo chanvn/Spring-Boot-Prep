d9a250c441c60ee1c2d67038fcd9870350aaf97d
chanv <chanvir.nijjar@gmail.com>
2021-09-21 21:11:51 -0400

    Updated HappyFortuneService.java

--- Annotations-1/src/main/java/com/springdemoannotations/demo/HappyFortuneService.java
+++ Annotations-1/src/main/java/com/springdemoannotations/demo/HappyFortuneService.java
@@ -3,6 +3,7 @@ package com.springdemoannotations.demo;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class HappyFortuneService implements FortuneService {
     @Override
     public String getFortune() {
